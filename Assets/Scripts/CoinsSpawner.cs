d89af904c82eed52b2bc5fcc11860b7a97ff38c6
gloryspb <spbbglory@gmail.com>
2023-04-29 02:45:53 +0600

    some fixes

---
+++ Assets/Scripts/CoinsSpawner.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinsSpawner : MonoBehaviour
+{
+    // public GameObject objectToSpawn; // префаб объекта, который нужно спавнить
+    // public int numberOfObjects; // количество объектов для спавна
+    // public float spawnRadius; // радиус, в котором должны спавниться объекты
+
+    // void Start()
+    // {
+    //     for (int i = 0; i < numberOfObjects; i++)
+    //     {
+    //         Vector3 randomPosition = transform.position + Random.insideUnitSphere * spawnRadius; // генерация случайной позиции в радиусе
+    //         Instantiate(objectToSpawn, randomPosition, Quaternion.identity); // создание объекта на случайной позиции
+    //     }
+    // }
+}
