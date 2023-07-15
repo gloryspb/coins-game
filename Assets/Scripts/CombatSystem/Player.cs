75feef5f73e554b741562db2b3446a71bf3fad37
gloryspb <spbbglory@gmail.com>
2023-07-15 19:14:15 +0300

    Update Player.cs

--- Assets/Scripts/CombatSystem/Player.cs
+++ Assets/Scripts/CombatSystem/Player.cs
@@ -40,14 +40,6 @@ public class Player : MonoBehaviour
 
         spriteRenderer.color = Color.red;
         Invoke("SetColorWhite", 0.25f);
-        // Получаем направление от источника урона к игроку
-        // Vector2 knockbackDirection = (Vector2)transform.position - (Vector2)enemyTransform.position;
-        // knockbackDirection.Normalize();
-        // Vector2 knockbackPosition = (Vector2)transform.position + knockbackDirection * knockbackDistance;
-
-        // Применяем отталкивающую силу в противоположном направлении
-        // rb.AddForce(knockbackDirection * knockbackForce, ForceMode2D.Impulse);
-        // rb.MovePosition(knockbackPosition);
     }
 
     void SetColorWhite()
@@ -59,19 +51,11 @@ public class Player : MonoBehaviour
     {
         _animator.SetTrigger("DeathTrigger");
         isDead = true;
-        // Time.timeScale = 0f;
-        // Invoke("DeathHandler", 1f);
         rb.isKinematic = false;
         _collider.enabled = false;
         StartCoroutine(InvokeMethodWithDelay());
     }
-	
-    // private void DestroyObject()
-    // {
-    //     // Destroy(gameObject);
-    //     Time.timeScale = 1f;
-    //     Time.timeScale = 0f;
-    // }
+
     IEnumerator InvokeMethodWithDelay()
     {
         yield return new WaitForSeconds(1f);
@@ -82,6 +66,5 @@ public class Player : MonoBehaviour
     {
         UIEventHandler.Instance.DeathScreen();
         Time.timeScale = 0f;
-        // Debug.Log("death");
     }
 }
